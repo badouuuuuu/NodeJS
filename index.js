9e550b416f45128b7171ec4eb1fac0d3a61d5cf0
badou <el.hirech.youssef@gmail.com>
2019-01-21 19:07:25 +0100

    new CLI mailcheckerCLI

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const axios = require('axios')
 const markdown = require('markdown-js')
 const validator = require('email-validator')
 const ora = require('ora');
-
+const chalk = require('chalk');
+const figlet = require('figlet');
 
 const [, , ...args] = process.argv
 const email = validator.validate(`${args}`)
@@ -14,14 +15,21 @@ const email = validator.validate(`${args}`)
 
 const url = `https://haveibeenpwned.com/api/v2/breachedaccount/${args}`
 
+
+console.log(chalk.white.bold(figlet.textSync('Email   Breach   Checker! \n', {
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+})));
+
+
+
 if (email == true) {
-    const spinner = ora(`Checking for Breach :${args}`);
+    const spinner = ora(`Checking for Breach : ` + chalk.red(`${args}`));
     spinner.start();
 
     setTimeout(() => {
-        spinner.color = 'yellow';
-        spinner.text = `Checking
-        for Breach on ${args}`;
+        spinner.color = 'red';
+        spinner.text = chalk.red(`\nBreach found for : \n`) + chalk.yellow(`${args}\n`);
 
 
         axios({
@@ -33,25 +41,36 @@ if (email == true) {
             }
         }).then(res => {
 
-            res.data.forEach(function(breach) {
-                console.log('Domaine : \n' +
-                    breach.Domain);
-                console.log('Breach Date : \n' +
-                    breach.BreachDate);
-                console.log();
-                console.log('Description : \n' +
-                    breach.Description);
-                console.log('----------------------------------- \n')
+            res.data.forEach(function (breach) {
+
+                console.log(chalk.red.bold('\nDomaine : ') +
+                    chalk.italic(breach.Domain + '\n' ));
+
+                console.log(chalk.red.bold('Breach Date : ') +
+                    chalk.italic(breach.BreachDate));
+
+                console.log(chalk.red.bold('Description : \n') +
+                    chalk.yellow.italic(breach.Description));
+
+                console.log(chalk.green('-----------------------------------\n'))
 
             })
 
-            spinner.stop();
+            console.log(chalk.green('Github : ') + 'https://github.com/badouuuuuu\nby badouuuuuu for Becode.Org\n')
+       
+       
         }).catch(err => {
-            const log = chalk.yellow(err)
-            console.log(log)
+
+            if(err.response.status == 404) {
+
+                console.log("\n-> No Breach Found, " +  chalk.green("Youhou!\n"));
+                console.log(chalk.italic('by badouuuuuu for Becode.Org\n'));
+                console.log('Github : https://github.com/badouuuuuu');
+            }
+       
         })
 
-
+        spinner.stop();
 
     }, 2000);
 
